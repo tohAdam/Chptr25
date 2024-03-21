bc1e2db2a00b72614c80ff9b877838335eaa76d5
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-03-21 09:50:57 +0800

    4

---
+++ MonkeysRhyme.java
@@ -0,0 +1,12 @@
+public class MonkeysRhyme {
+    public static void main(String[] args) {
+        for (int count = 5; count >= 1; count--) {
+            System.out.println(count + " little monkeys swinging in a tree");
+            System.out.println("Teasing Mr Crocodile \"you can't catch me\"");
+            System.out.println("Along came the crocodile as quiet as can be");
+            System.out.println("and SNAP!\n");
+        }
+        System.out.println("No little monkeys swinging in a tree");
+        System.out.println("No little monkeys swinging in a tree");
+    }
+}
